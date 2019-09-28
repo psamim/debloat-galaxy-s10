0eab228600aaa751dcbad95a246aaf23c26a0548
Samim Pezeshki <psamim@gmail.com>
2019-09-28 13:40:39 +0330

    Update generateWhatRemoved

--- generateWhatRemoved.sh
+++ generateWhatRemoved.sh
@@ -1,3 +1,7 @@
 adb shell pm list packages -u > all.packages
 adb shell pm list packages > now.packages
-sort all.packages now.packages | uniq -u
+rm -f reinstall.sh
+sort all.packages now.packages | uniq -u > reinstall.sh
+sed -i -e 's/package://' reinstall.sh
+sed -i -e 's/^/adb shell pm install-existing /' reinstall.sh
+chmod +x reinstall.sh
